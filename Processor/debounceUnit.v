d398f74fb3c33050fe345e4cfa89ea7f2f981833
AdamEbrahim <110650531+AdamEbrahim@users.noreply.github.com>
2023-12-11 00:03:28 -0500

    debounce changes

--- Processor/debounceUnit.v
+++ Processor/debounceUnit.v
@@ -4,28 +4,48 @@ module debounceUnit(buttonNotPressed, acknowledge, clock, debounce);
     input buttonNotPressed, clock;
     output reg debounce;
 
-    reg reset_counter;
+    //reg reset_counter;
     initial begin
         debounce = 1'b0;
-        reset_counter = 1'b0;
+        //reset_counter = 1'b0;
     end
 
-    wire debounceCounterDone;
+    //wire debounceCounterDone;
 
-    always @(posedge clock or posedge debounceCounterDone) begin
-        if (debounceCounterDone & debounce) begin
-            debounce = 1'b0;
-        end else if ((~buttonNotPressed) & ~(debounce)) begin
+    // always @(posedge clock or posedge debounceCounterDone) begin
+    //     if (debounceCounterDone & debounce) begin
+    //         debounce = 1'b0;
+    //     end else if ((~buttonNotPressed) & ~(debounce)) begin
+    //         if (acknowledge == 1) begin
+    //             reset_counter = 1'b1;
+    //             debounce = 1'b1;
+    //             #10;
+    //             reset_counter = 1'b0;
+    //         end
+    //     end
+    // end
+
+
+    reg [25:0] counter_reg = 0;
+    reg timerStart = 0;  
+
+    always @(posedge clock) begin
+        if ((~buttonNotPressed) & ~(debounce)) begin
             if (acknowledge == 1) begin
-                reset_counter = 1'b1;
                 debounce = 1'b1;
-                #10;
-                reset_counter = 1'b0;
+                timerStart = 1'b1;
             end
+        end else if (timerStart) begin
+            timerStart = 1'b0;
+            counter_reg = 0;
+        end else if (counter_reg == 15000000) begin
+            debounce = 1'b0;
+        end else if (debounce) begin
+            counter_reg <= counter_reg + 1;
         end
     end
 
-    debounce_counter DEBOUNCE(.clock(clock), .counter_done(debounceCounterDone), .reset(reset_counter));
+    //debounce_counter DEBOUNCE(.clock(clock), .counter_done(debounceCounterDone), .reset(reset_counter));
 
 
 endmodule
